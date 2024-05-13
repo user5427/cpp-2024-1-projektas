d72fc74d56093e4af01e07c44ce4ab4b01cc67bf
Arnas Šniokaitis <arnas.sniokaitis@mif.stud.vu.lt>
2024-05-13 08:18:51 +0300

    Klaidos taisymas

--- Backend/FileIO.cpp
+++ Backend/FileIO.cpp
@@ -9,24 +9,16 @@ struct FileIO::FileIOImpl{
     std::string Name;
     std::fstream File;
 };
-
 FileIO::FileIO(std::string fileName) {
     ptr = new struct FileIOImpl;
     ptr->Name = fileName;
     //ptr->File.open(fileName, std::ios::in | std::ios::out | std::ios::trunc);
 }
-
-
 std::vector<std::string> FileIO::readAll() {
     std::vector<std::string> data;
     std::string inf;
     ptr->File.open(ptr->Name, std::fstream::in);
-    while(std::getline(ptr->File, inf)) {
-        char TMP[50001];
-        //ptr->File.getline(TMP, 50000);
-
-
-        //std::string inf(TMP);
+    while(std::getline(ptr->File, inf)){
         data.push_back(inf);
     }
     ptr->File.close();
